56dd2e504691e1015b2cb558a5ebbf779893b1cf
Pedro Rojas <pedro.rojaslo50@gmail.com>
2024-03-04 21:31:29 -0800

    Initial Commit

---
+++ OnlineStoreMV/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OnlineStoreMV
+//
+//  Created by Pedro Rojas on 04/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OnlineStoreMV/OnlineStoreMVApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnlineStoreMVApp.swift
+//  OnlineStoreMV
+//
+//  Created by Pedro Rojas on 04/03/24.
+//
+
+import SwiftUI
+
+@main
+struct OnlineStoreMVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
