377afe49d712ef69bede4025086244f077ea1f92
Pedro Rojas <pedro.rojaslo50@gmail.com>
2024-10-01 10:48:07 -0700

    Add UI Test Template

---
+++ OnlineStoreMV/Test/UI/OnlineStoreMV_UITests.swift
@@ -0,0 +1,40 @@
+//
+//  OnlineStoreMV_UITests.swift
+//  OnlineStoreMVUITests
+//
+//  Created by Pedro Rojas on 01/10/24.
+//
+
+import XCTest
+
+final class OnlineStoreMV_UITests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testLoadingProductList() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        
+    }
+
+    func testLaunchPerformance() throws {
+        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
+            // This measures how long it takes to launch your application.
+            measure(metrics: [XCTApplicationLaunchMetric()]) {
+                XCUIApplication().launch()
+            }
+        }
+    }
+}
